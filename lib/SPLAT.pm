1a42e9242bed0fe6b7c94e4bf608218b5ab5a99d
Dave Ackley <ackley@ackleyshack.com>
2018-11-26 20:46:41 -0700

    v0.04

--- lib/SPLAT.pm
+++ lib/SPLAT.pm
@@ -10,11 +10,11 @@ App::Splattr - Translator for SPLAT (Spatial Programming Language, Ascii Text)
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
