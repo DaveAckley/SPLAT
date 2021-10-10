fe562a69c0ed8a93790fd8812877e42d5e2301a0
Dave Ackley <ackley@livingcomputation.org>
2021-10-10 14:01:51 -0600

    Try doing without diagnostics

--- lib/SPLATtr.pm
+++ lib/SPLATtr.pm
@@ -1,6 +1,7 @@
 use strict;
 use warnings;
-use diagnostics;
+# 202110101359 Do we need this? it's messing with horrible borrible appimage-builder
+# use diagnostics;
 use Carp;
 use Data::Dumper;
 
